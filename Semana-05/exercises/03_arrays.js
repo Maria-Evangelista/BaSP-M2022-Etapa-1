44c605d5f6d728f4fe11c89b7f59478c42c60940
maria evangelista <evangelista.maria13@gmail.com>
2022-04-18 16:56:18 -0300

    BaSP-2022: ex03-arrays

---
+++ Semana-05/exercises/03_arrays.js
@@ -0,0 +1,47 @@
+console.log("EXERCISE 3: ARRAYS");
+
+/* a. Dado el siguiente array: 
+["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", 
+"Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"] 
+mostrar por consola los meses 5 y 11 (utilizar console.log). */
+
+console.log("- Exercise 3.a: ");
+var months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
+"Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+console.log("The fifth month is", months[5-1], "and the eleventh month is", months[11-1]);
+
+/* b. Ordenar el array de meses alfabéticamente y mostrarlo por consola 
+(utilizar sort). */
+console.log("- Exercise 3.b: ");
+console.log(months.sort());
+
+/* c. Agregar un elemento al principio y al final del array 
+(utilizar unshift y push). */
+console.log("- Exercise 3.c: ");
+months.unshift("2021");
+months.push("2022");
+console.log(months);
+
+/* d. Quitar un elemento del principio y del final del array 
+(utilizar shift y pop). */
+console.log("- Exercise 3.d: ");
+months.shift();
+months.pop();
+console.log(months);
+
+/* e. Invertir el orden del array 
+(utilizar reverse). */
+console.log("- Exercise 3.e: ");
+console.log(months.reverse());
+
+/* f. Unir todos los elementos del array en un único string donde cada mes este separado por un guión - 
+(utilizar join). */
+console.log("- Exercise 3.f: ");
+console.log(months.join("-"));
+
+/* g. Crear una copia del array de meses que contenga desde Mayo hasta Noviembre 
+(utilizar slice). */
+console.log("- Exercise 3.g: ");
+var months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
+"Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+console.log(months.slice(4,11));
